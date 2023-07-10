27dc0b70c89664fa4d24c974ae78549ba154f7d6
pabongamas <pabongamas@gmail.com>
2023-07-10 15:48:40 -0500

    actualizacion general proyecto platzi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +10,18 @@ import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
-import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [
@@ -29,11 +36,21 @@ import { environment } from './../environments/environment';
     CoreModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    AngularFireStorageModule
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
   ],
-  providers: [],
+  providers: [
+  
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,20 @@
 export const environment = {
-  production: true,
-  url_api: 'https://store.nicobytes.site/api',
   firebase: {
-    apiKey: 'AIzaSyCN599W6_flBEpe0xrcuBlGPRW3OKzDr7o',
-    authDomain: 'platzi-store-forms.firebaseapp.com',
-    databaseURL: 'https://platzi-store-forms.firebaseio.com',
-    projectId: 'platzi-store-forms',
-    storageBucket: 'platzi-store-forms.appspot.com',
-    messagingSenderId: '783669493891',
-    appId: '1:783669493891:web:f973c1ba9bb213e2c75bb6'
-  }
+    projectId: 'yarnstore-3c33f',
+    appId: '1:867128090595:web:964447a0696ef539494ad6',
+    storageBucket: 'yarnstore-3c33f.appspot.com',
+    apiKey: 'AIzaSyBg3bIxmr4S0dytWZI0CkZsQMb3QplomFw',
+    authDomain: 'yarnstore-3c33f.firebaseapp.com',
+    messagingSenderId: '867128090595',
+  },
+  production: true,
+  // url_api: 'https://store.nicobytes.site/api',
+  // firebase: {
+  //   projectId: 'yarnstore-3c33f',
+  //   appId: '1:867128090595:web:964447a0696ef539494ad6',
+  //   storageBucket: 'yarnstore-3c33f.appspot.com',
+  //   apiKey: 'AIzaSyBg3bIxmr4S0dytWZI0CkZsQMb3QplomFw',
+  //   authDomain: 'yarnstore-3c33f.firebaseapp.com',
+  //   messagingSenderId: '867128090595',
+  // }
 };
