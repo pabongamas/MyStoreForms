4ddb9f9aba3966c878528e42044d7afad48dc075
pabongamas <pabongamas@gmail.com>
2023-07-10 14:19:55 -0500

    actualizacion 16

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 
@@ -8,7 +8,7 @@ import { AuthService } from './core/services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard implements CanActivate {
+export class AdminGuard  {
 
   constructor(
     private authService: AuthService,
