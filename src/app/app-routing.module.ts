9c5d1fd73828e2d6f36d8167b8b714a7c875d58a
pabongamas <pabongamas@gmail.com>
2023-07-10 12:05:39 -0500

    commit 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -54,8 +54,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules,
-    relativeLinkResolution: 'legacy'
+    preloadingStrategy: PreloadAllModules
 })],
   exports: [RouterModule]
 })
