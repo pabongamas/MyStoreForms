ce23bbac554c545a689bff0ccd599b8ee92610e9
pabongamas <72658365+pabongamas@users.noreply.github.com>
2023-07-10 22:55:32 -0500

    angular forms validations

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
   styleUrls: ['./basic-form.component.scss']
 })
-export class BasicFormComponent {
+export class BasicFormComponent implements OnInit{
+  nameField=new  FormControl('',[Validators.required,Validators.maxLength(10)]);
+  emailField=new FormControl('');
+  phoneField=new FormControl('');
+  colorField=new FormControl('#213b87');
+  dateField=new FormControl('');
+  numberField=new FormControl('12');
 
+  categoryField=new FormControl('category-3');
+  tagField=new FormControl('');
+
+  agreeField=new FormControl(false);
+  genderField=new FormControl('');
+  zoneField=new FormControl('');
+
+  ngOnInit(): void {
+    this.nameField.valueChanges
+    .subscribe(value=>{
+      console.log(value);
+    })
+  }
+
+  getNameValue(){
+    console.log(this.nameField.value);
+  }
 }
