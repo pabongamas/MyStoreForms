af0bf18f3dc913e2d0d140004fbbe1b8f6cc7de3
pabongamas <72658365+pabongamas@users.noreply.github.com>
2023-07-14 23:26:17 -0500

    validaciones personalizadas y condicionadas

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -40,7 +40,29 @@ export class RegisterComponent implements OnInit {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required]],
       password: ['', [Validators.required,Validators.minLength(6),MyValidators.validatePassword]],
+      confirmPassword:['',[Validators.required]],
+      type:['company',[Validators.required]],
+      companyName:['',[Validators.required]],
+    },{
+      validators:MyValidators.matchPasswords
     });
+
+    this.typeField.valueChanges
+    .subscribe(value=>{
+      console.log(value);
+      if(value==="company"){
+        this.companyNameField.setValidators([Validators.required]);
+      }else{
+        this.companyNameField.setValidators(null);
+      }
+      this.companyNameField.updateValueAndValidity();
+    })
+  }
+  get typeField(){
+    return this.form.get('type');
+  }
+  get companyNameField(){
+    return this.form.get('companyName');
   }
 
 }
